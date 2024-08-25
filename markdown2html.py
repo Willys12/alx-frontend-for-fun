55d8f6df8943ca8881f6b0ee5d3bb7fa2189b269
Willys12 <ben.willys92@gmail.com>
2024-08-25 13:31:38 +0300

    Start a script correction

--- markdown2html.py
+++ markdown2html.py
@@ -2,7 +2,7 @@
 """
 A script that takes an argument 2 strings:
 First argument is the name of the Markdown file.
-Second argument is the output file name.
+Second argument is the output file name..
 """
 import sys
 import os.path
