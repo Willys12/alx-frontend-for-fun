11278caaa10812a97cbfca2ccee2e39f6fc2a8d0
Willys12 <ben.willys92@gmail.com>
2024-08-25 13:27:31 +0300

    Start a script correction

--- markdown2html.py
+++ markdown2html.py
@@ -5,10 +5,10 @@ First argument is the name of the Markdown file.
 Second argument is the output file name.
 """
 import sys
-import os
+import os.path
 
 
-def main():
+if __name__ == "__main__":
     """Main function of the script.
     Read the Markdown file, convert it to HTML, and save it to the output file.
     """
@@ -17,15 +17,8 @@ def main():
               file=sys.stderr)
         exit(1)
 
-    markdown_file = sys.argv[1]
-    output_file = sys.argv[2]
-
-    if not os.path.isfile(markdown_file):
-        print(f'Missing {markdown_file}', file=sys.stderr)
+    if not os.path.isfile(sys.argv[1]):
+        print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
         exit(1)
 
     exit(0)
-
-
-if __name__ == '__main__':
-    main()
