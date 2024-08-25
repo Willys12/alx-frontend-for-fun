fc5e15a9565fa047f471c09c177b7322674c6911
Willys12 <ben.willys92@gmail.com>
2024-08-25 13:13:40 +0300

    Start a script

--- markdown2html.py
+++ markdown2html.py
@@ -7,23 +7,22 @@ Second argument is the output file name.
 import sys
 import os
 
+
 def main():
     """Main function of the script.
     Read the Markdown file, convert it to HTML, and save it to the output file.
     """
     if len(sys.argv) < 3:
-        print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
+        print('Usage: ./markdown2html.py README.md README.html',
+              file=sys.stderr)
         exit(1)
-
 
     markdown_file = sys.argv[1]
     output_file = sys.argv[2]
 
-
     if not os.path.isfile(markdown_file):
         print(f'Missing {markdown_file}', file=sys.stderr)
         exit(1)
-
 
     exit(0)
 
